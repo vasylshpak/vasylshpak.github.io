a87d156c6371f38702465398bd7106aa432558f5
Vasyl Shpak <vasyl.shpak@grandparade.co.uk>
2021-08-21 12:40:43 +0200

    init

---
+++ build/precache-manifest.b2f3549ae1620741e39943b99bad2e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83a8b7dfcc07f207119bc50a8e9f563",
+    "url": "/vasyl-shpak-cv/index.html"
+  },
+  {
+    "revision": "6abc91cf9a962523372f",
+    "url": "/vasyl-shpak-cv/static/css/main.c470b900.chunk.css"
+  },
+  {
+    "revision": "d5daa63b8ee15894c6f1",
+    "url": "/vasyl-shpak-cv/static/js/2.93f44c7b.chunk.js"
+  },
+  {
+    "revision": "6abc91cf9a962523372f",
+    "url": "/vasyl-shpak-cv/static/js/main.f50589c2.chunk.js"
+  },
+  {
+    "revision": "c4297b5990708e3a23a7",
+    "url": "/vasyl-shpak-cv/static/js/runtime-main.51dbc622.js"
+  }
+]);
